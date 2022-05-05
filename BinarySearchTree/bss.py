3b302e7130b525fffafbe83db77a2d9d4453cb75
gcasa032 <46498819+gcasa032@users.noreply.github.com>
2022-05-05 19:56:00 +0000

    added bst delete

--- BinarySearchTree/bss.py
+++ BinarySearchTree/bss.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Node:
     ''' A binary search tree node'''
     def __init__(self, val= None):
@@ -25,7 +28,61 @@ class BinarySearchTree:
     '''Implements many Binary Search Tree functions'''
 
     def __init__(self, key = None):
-        self.root = Node(key)
+        if key is not None:
+            self.root = Node(key)
+        else:
+            self.root = None
 
     def insert(self, key):
-        self.root.insert(key)
+        ''' Insert key into tree '''
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self.root.insert(key)
+
+    def find(self, key):
+        '''Returns true if value is found in key, false if not'''
+        temp = self.root
+
+        while temp:
+            if temp.val < key:
+                temp = temp.right
+            elif temp.val > key:
+                temp = temp.left
+            else:
+                return True
+
+        return False
+
+    def delete(self, root: Node, key):
+        ''' Deletes node = key. Returns true if deletion was successful and false if not'''    
+        if root is None:
+            return root
+        elif key < root.val:
+            root.left = self.delete(root.left, key)
+        elif key > root.val:
+            root.right = self.delete(root.right, key)
+        else:                     
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            #if it has two children??
+
+
+tree = BinarySearchTree()
+
+for x in range(100):
+    val = random.randint(48, 999999)
+    tree.insert(val)
+
+tree.insert(47)
+
+print(tree.find(47))
+tree.delete(tree.root, 47)
+print(tree.find(47))
--- BinarySearchTree/bss_main.py
+++ BinarySearchTree/bss_main.py
@@ -5,5 +5,10 @@ tree = BinarySearchTree()
 
 for x in range(100):
     val = random.randint(0, 999999)
-    print(val)
     tree.insert(val)
+
+tree.insert(47)
+
+print(tree.find(47))
+print(tree.delete(47))
+print(tree.find(47))
